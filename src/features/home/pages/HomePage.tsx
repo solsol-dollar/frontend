bd481d6f726a3aa4e6b1120dca090bee293608e3
heessunny <kimheesun107@gmail.com>
2026-06-16 15:43:59 +0900

    fix: img alt 추가

--- src/features/home/pages/HomePage.tsx
+++ src/features/home/pages/HomePage.tsx
@@ -40,7 +40,7 @@ export function HomePage() {
           onClick={() => {}}
         >
           <div className='bg-blue-100 px-2 py-2 rounded-3xl'>
-          <span className="text-2xl leading-none"><img src={sleepingIcon} /></span>
+          <span className="text-2xl leading-none"><img src={sleepingIcon} alt="쉬는 달러 감지 아이콘"/></span>
           </div>
           <div className="flex-1 min-w-0">
             <p className="text-sm font-semibold text-text-primary">쉬는 달러 감지</p>
